3aac10c1b653c4b2309dc7dc0e81c8966ed68c6b
Liam Murphy <liammurphy513@gmail.com>
2022-09-12 16:23:20 -0500

    adding first cut of research page content

--- src/components/Research.js
+++ src/components/Research.js
@@ -1,13 +1,78 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import { connect } from "react-redux";
+import * as constants from "../constants";
 
 class Research extends React.Component {
+    
+    displayResearchContent() {
+        if (this.props.winWidth < constants.BREAK_WIDTH) {
+            return (
+                <></>
+            );
+        } else {
+            return (
+                <>
+                    <Grid className="grid-container" style={{columnGap: "4rem"}} container>
+                        <Grid item xs={5}>
+                            <p>
+                                <u>Interests:</u> Programming languages, compilers, interpreters, embedded systems,
+                                data ethics, and cybersecurity.
+                            </p>
+                            
+                            <p>
+                                My master's thesis, titled <i>Improving Compiler Construction Education by Retargeting 
+                                and Extending a Compiler for Embedded Xinu</i>, deals with improving the pedagogical compiler 
+                                used to instruct undergraduate and graduate students enrolled in Marquette University's 
+                                COSC 4400/5400 Compiler Construction course.
+                            </p>
+
+                            <p>
+                                I argue that, by retargeting the compiler to the most recent Embedded Xinu platform, students 
+                                can draw upon previously acquired skills in operating systems to increase their success in learning 
+                                concepts related to compiler construction.
+                            </p>
+
+                            <p>
+                                While working under the supervision of <a href="https://www.cs.mu.edu/~brylow" target="_blank" rel="noreferrer">Dr. Dennis Brylow</a>, I maintained a {" "}
+                                <a href="https://www.cs.mu.edu/~lmurphy9/research" target="_blank" rel="noreferrer">research log</a> on my academic
+                                website to track this project's progress.
+                            </p>
+
+                            <p>
+                                This thesis was successfully defended on April 1st, 2022. The full-text document can be found on {" "}
+                                <a href="https://www.proquest.com/docview/2658279769/9925D60F56E04A36PQ/15?accountid=100&parentSessionId=a%2FLZvm5eV3O0fikN3IKrciJ4ceOR73KnPdsqWu3tpOA%3D" target="_blank" rel="noreferrer">ProQuest</a> 
+                                , with access provided through Marquette University. You can also view the full PDF <a href="../assets/LiamMurphyResume2022.pdf" target="_blank" rel="noreferrer">here</a>.
+                            </p>
+
+                            <p>
+                                During the summer of 2019, I worked under the guidance of Dr. Dennis Brylow as an undergraduate research assistant for the 
+                                Systems Laboratory. I helped to port a framebuffer device driver to the Raspberry Pi 3 B+ running Embedded Xinu. After that 
+                                project was completed, I began development on a window abstraction device driver and a shell multiplexer similar to the popular
+                                Linux utility <code>tmux</code>. This helped to expand the team's knowledge on the framebuffer device as well as working with Xinu's shell.
+                            </p>
+                        </Grid>
+                    </Grid>
+                </>
+            )
+        }
+    }
+    
     render() {
         return (
             <div className="page-content-container">
-                <h1>Hello</h1>
+                <h2 className="page-title">Research Experience</h2>
+                {this.displayResearchContent()}
             </div>
         );
     }
 }
 
-export default Research;
+const mapStateToProps = (state) => {
+    return {
+        winHeight: state.winHeight,
+        winWidth: state.winWidth
+    }
+};
+
+export default connect(mapStateToProps)(Research);
