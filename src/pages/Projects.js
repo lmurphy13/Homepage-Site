fc93f4499e3790a6be15f504c9fd642c3f0c4a4a
Liam Murphy <liammurphy513@gmail.com>
2024-04-05 22:24:27 -0500

    wrapping up the pc build article. i might revise it later

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,10 +13,10 @@ class Projects extends React.Component {
 
                 <div className="project-article-container">
                     <div className="project-article-card">
-                        <span className="project-article-card-title">Linux PC Build</span>
+                        <span className="project-article-card-title">2024 Linux PC Build</span>
                         <p className="project-article-card-desc">
                             My latest PC build. I'm running Fedora 39 on an Intel i9 with two graphics cards.
-                            One is configured for PCIe passthrough to a QEMU/KVM VM running Windows 11 for gaming.
+                            One is configured for PCI passthrough to a QEMU/KVM VM running Windows 11 for gaming.
                         </p>
                         <span>Link to <Link to="/projects/pc-build">page</Link></span>
                     </div>
@@ -33,7 +33,8 @@ class Projects extends React.Component {
                     <div className="project-article-card">
                         <span className="project-article-card-title">LBASIC Compiler</span>
                         <p className="project-article-card-desc">
-                            Written in C, LBASIC is a simple, compiled language with syntax inspired by C and BASIC.
+                            Written in C, LBASIC is a simple, compiled language with syntax inspired by C and BASIC. This project is in
+                            semi-active development. I come back to it every few months.
                         </p>
                         <span><a href="https://github.com/lmurphy13/LBASIC.git"><GitHubIcon style={{color: "white"}}/></a></span>
                     </div>
