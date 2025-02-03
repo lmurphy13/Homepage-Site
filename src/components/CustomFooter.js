9cd5df4a3b0ce9aa18b6713be23c11f94bf3fcb6
Liam Murphy <liammurphy513@gmail.com>
2025-02-02 21:05:01 -0600

    updating copyright year

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2024 - Liam M. Murphy */
+/* Copyright (c) 2024, 2025 - Liam M. Murphy */
 
 import React from "react";
 
@@ -6,7 +6,7 @@ class CustomFooter extends React.Component {
     render() {
         return (
             <div className="custom-footer">
-                <p>Copyright &copy; 2019-2024 &sdot; Liam M. Murphy</p>
+                <p>Copyright &copy; 2019-2025 &sdot; Liam M. Murphy</p>
             </div>
         );
     }
