2e3b5421e588e6db611faafddd3ea2ae397435a0
Liam Murphy <liammurphy513@gmail.com>
2025-05-04 20:00:58 -0500

    updating homepage image and sitewide captions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
-/* Copyright (c) 2024 - Liam M. Murphy */
+/* Copyright (c) 2024-2025 - Liam M. Murphy */
 
 import React from "react";
-import liam from "../media/liam-2023-3.png";
+import liam from "../media/LiamAH_cropped.png";
 
 class Home extends React.Component {
     render() {
         return (
             <div className="page-content">
-                <img className="custom-image" src={liam} alt="liam"></img>
-                <p>
+                <img className="custom-image-home" src={liam} alt="Liam at Airbus Helicopters, Marignane, France"></img>
+                <caption className="image-caption">Airbus Helicopters, Marignane, France</caption>
+                <p style={{"paddingTop":"2rem"}}>
                     I am a software engineer living in Chicago. I work on
                     avionics connectivity equipment at{" "}
                     <a href="https://astronautics.com">
